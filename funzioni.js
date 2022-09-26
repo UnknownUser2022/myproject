fc90a1edc0eaed601bae435d5c7dee87836f75c4
ZioMark <114426017+UnknownUser2022@users.noreply.github.com>
2022-09-26 18:06:08 +0200

    Upload
    Upload

---
+++ funzioni.js
@@ -0,0 +1,21 @@
+//FUNZIONI LOGIN
+$(document).ready(function () {
+  if (localStorage.getItem("inputvalue")) {
+    $("#nomescelto").text(localStorage.getItem("inputvalue"));
+    //$('.home').text(localStorage.getItem("inputvalue"));
+  }
+
+  $("#login-form-submit").on("click", function () {
+    localStorage.setItem("inputvalue", $("#username-field").val());
+    $("#nomescelto").text(localStorage.getItem("inputvalue"));
+    //$('.home').text(localStorage.getItem("inputvalue"));
+  });
+
+  $("#pulisci").on("click", function () {
+    localStorage.removeItem("inputvalue");
+  });
+
+  $("#puliscitutto").on("click", function () {
+    localStorage.clear();
+  });
+});
